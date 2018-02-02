030d235c8d56de3f736a5e1f2554345ea20caa48
Dom Hede <dom@bridgeapp.com.au>
2018-02-02 22:06:57 +1100

    Add handler for Fetch Properties action

--- src/data/reducers/properties.js
+++ src/data/reducers/properties.js
@@ -1,10 +1,18 @@
+import { FETCH_PROPERTIES } from '../../constants/actionTypes'
+
 const INITIAL_STATE = {
-  properties: {},
-  savedProperties: {}
+  properties: [],
+  savedProperties: []
 };
 
-export default function todos(state = INITIAL_STATE, action) {
+export default function properties(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case FETCH_PROPERTIES:
+      return {
+        properties: action.payload,
+        ...state
+      }
+
     default:
       return state
   }
--- src/data/reducers/properties.test.js
+++ src/data/reducers/properties.test.js
@@ -1,12 +1,26 @@
 import reducer from './properties';
+import * as types from '../../constants/ActionTypes';
+import data from '../data.json';
 
 describe('properties reducer', () => {
   it('should return the initial state', () => {
     expect(reducer(undefined, {})).toEqual(
       {
-        properties: {},
-        savedProperties: {}
+        properties: [],
+        savedProperties: []
       }
     )
   })
+  it('should handle FETCH_PROPERTIES', () => {
+    expect(
+      reducer([], {
+        type: types.FETCH_PROPERTIES,
+        payload: data.results
+      })
+    ).toEqual(
+      {
+        properties: data.results
+      }
+    )
+  });
 });
